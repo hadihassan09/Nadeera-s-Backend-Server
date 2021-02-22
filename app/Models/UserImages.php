5e244be84f5b956364ddf9164a6bc55f5b980dc7
Hadi Hassan <hadihassan09@gmail.com>
2021-02-22 13:55:21 +0200

    added elequent model relation ship function:

--- app/Models/UserImages.php
+++ app/Models/UserImages.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class UserImages extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category',
+        'uri',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
